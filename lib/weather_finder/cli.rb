841870908d6aba1fd1c7f14e79e3399a0cd0362d
Justin Tooley <jstooley823@gmail.com>
2017-09-23 18:01:45 +0000

    added methods to WeatherFinder::CLI

--- lib/weather_finder/cli.rb
+++ lib/weather_finder/cli.rb
@@ -1,5 +1,20 @@
 class WeatherFinder::CLI
 
   def call
+  end # call method end
+
+  def deals(deals)# prints all deals of a chosen site
+  end
+
+  def deal_info(choice)
+     # will print the chosen deals info
+  end# end of deal_info
+
+  def menu(site_choice)
+  end # end menu method
+
+  def goodbye # prints goodbye messages and exits program
+    puts "cya later nerd!"
+    exit
   end
 end
